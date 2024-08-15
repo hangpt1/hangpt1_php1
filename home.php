4cbfa5a61c912e18990beba5691b191972fc5698
hangpt1 <phamhang1222004@gmail.com>
2024-08-16 06:55:56 +0700

    hangpt php1

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./main.css">
+    <title>Trang gioi thieu</title>
+
+</head>
+<body>
+<?php
+echo '<center><a target="_blank" href="https://www.facebook.com/profile.php?id=100013363294385&locale=vi_VN"><img class="img" src="./anh.jpg"></a></center>';
+echo '<center>
+        <p>Họ và tên: Phạm Thị Hằng</p>
+        <p>Lớp: K58SN2, Trường đại học Thương Mại</p>
+    <center>';
+
+echo '<button><a target="_blank"  href="https://github.com/hangpt1">Link Github</a></button>';
+?>
+</body>
+</html>
